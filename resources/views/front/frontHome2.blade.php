eedc0fe91ff45baa9c6a7b863ed6baa72753ce20
lutan0081 <lutan0081.h@gmail.com>
2022-05-08 03:30:20 +0900

    frontHome変更中

--- resources/views/front/frontHome2.blade.php
+++ resources/views/front/frontHome2.blade.php
@@ -213,7 +213,7 @@
                                 
                                     <div class="document_contents">
                                         <span class="f_document_jp">効率的に仕事をしたくないですか？<br></span>
-                                        <a href="#" class="btn btn-register box zoomInTrigger"><i class="far fa-gem me-2"></i>今すぐ資料請求</a><br>
+                                        <a href="" class="btn btn-register box zoomInTrigger"><i class="far fa-gem me-2"></i>今すぐ資料請求</a><br>
                                         <span class="f_document_jp_bottom">今までの「非効率」を「効率的」に<br></span>
                                     </div>
 
