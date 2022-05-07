669f51a2e92c16f9b827bf2b34380b4f2b741037
lutan0081 <lutan0081.h@gmail.com>
2022-05-08 04:17:58 +0900

    frontHome変更中

--- resources/views/component/front_register.blade.php
+++ resources/views/component/front_register.blade.php
@@ -9,7 +9,7 @@
                     
                         <div class="resi_contents">
                             <span class="f_resister_jp">今すぐ無料登録<br></span>
-                            <span class="f_resister_en">Let's register now<br></span>
+                            <span class="f_resister_en">Change Your Diary Task<br></span>
                             <a href="frontUserInit" class="btn btn-register box zoomInTrigger mt-5"><i class="far fa-gem me-2"></i>無料登録</a><br>
                         </div>
 
